a55746cbe6a5462dec029b572e64f1765afc207f
Manu Lannoy <manuel@webdesignord.fr>
2015-03-14 15:23:00 +0100

    boucle réglée mais facon bourin... :/

--- firstEvt_reveal.js
+++ firstEvt_reveal.js
@@ -1,31 +1,56 @@
 //(function () {// IEF protege variables})();// END IEF
 
-  function fade_show(divId,lapsToShow) {
+  function fade_show(divId, speed) {
     $(divId)
     .css({'display':'block'})
     .animate({
       opacity: 1
-    },lapsToShow);
-  }
-  var tabDia = $('#dia_container a'), tabDiaL = tabDia.length;
+    }, speed);
+  };
+  function inc_zindex(divId, speed, zIndex1) {
+    $(divId)
+    .animate({opacity:0}, 0)
+    .css('z-index','2')
+    .animate({
+      opacity: 1
+    }, speed);
+    $(zIndex1).css('z-index','1')
+  };
+  var tabDia = $('#dia_container a'),
+  tabDiaL    = tabDia.length;
+
   for (var i = 0; i < tabDiaL; i++) {
     tabDia[i].style.display = 'none';
     tabDia[i].style.opacity = '0';
   }
-  var tab1 = []; tab1.push(tabDia[0],tabDia[2]);
-  var tab2 = []; tab2.push(tabDia[1],tabDia[3]);
+  var tab1 = [],
+  tab2     = [];
+  tab1.push(tabDia[0],tabDia[2]);
+  tab2.push(tabDia[1],tabDia[3]);
 
   fade_show(tab1,1000);
 
   function showNextTab (argument) {
-    if(tab1[0].style.display == 'block' && tab2[0].style.display == 'none'){
-      fade_show(tab2,1000)
+    if(tab1[0].style.display == 'block' && tab2[0].style.display == 'none' && tab2[0].style.zIndex == ''){
+      fade_show(tab2,1000);
+      console.log('et maintenant ?');
+      setTimeout("showNextTab()", 3000);
     }else{
-      fctStart();
+      inc_zindex(tab1,1000,tab2);
+      console.log("tab1=opacity:0> z-index2 + opacity:1");
+
+      setTimeout("toggle_zIndex()", 3000)
     }
   };
+  function toggle_zIndex () {
+    if(tab2[0].style.zIndex == 1){
+      inc_zindex(tab2,1000,tab1);
+      setTimeout("toggle_zIndex()", 3000)
+    }else{
+      inc_zindex(tab1,1000,tab2);
+      console.log('ici?')
+      setTimeout("toggle_zIndex()", 3000)
+    }
+  }
 
-  var timeOut = setTimeout("showNextTab()", 3000);
-
-
-
+  setTimeout("showNextTab()", 3000);
